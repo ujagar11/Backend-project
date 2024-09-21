e223eb01eaa6af987adeeb596dddd9d0ae648b71
ujagar11 <ujagar.xi@gmail.com>
2024-09-21 09:52:21 +0530

    commit

--- src/main/java/com/BackendProject/Shopeeee/security/user/ShopUserDetailsService.java
+++ src/main/java/com/BackendProject/Shopeeee/security/user/ShopUserDetailsService.java
@@ -16,7 +16,7 @@ public class ShopUserDetailsService implements UserDetailsService  {
 
     private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = Optional.ofNullable(userRepository.findByEmail(email))
                .orElseThrow(()-> new  UsernameNotFoundException("user not found"));
 
